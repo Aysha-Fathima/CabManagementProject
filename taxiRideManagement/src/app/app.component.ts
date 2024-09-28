164dab6ccdd43f1eaabb8c2a79104bb84efa515a
aysha-fathima <fathimaayesha994@gmail.com>
2024-09-28 12:23:24 +0530

    angular modified

--- taxiRideManagement/src/app/app.component.ts
+++ taxiRideManagement/src/app/app.component.ts
@@ -1,10 +1,17 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RouterModule,NgFor,MatButtonModule,
+    MatInputModule,MatIconModule,MatTabsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- taxiRideManagement/src/app/app.routes.ts
+++ taxiRideManagement/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutusComponent } from '../components/aboutus/aboutus.component';
+import { HomeComponent } from '../components/home/home.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { LoginComponent } from '../components/login/login.component';
+import { RegisterComponent } from '../components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',component:HomeComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'aboutus',component:AboutusComponent},
+    {path:'login',component:LoginComponent},
+    {path:'register',component:RegisterComponent},
+    {path:'',redirectTo:'/home',pathMatch:'full'},
+];
