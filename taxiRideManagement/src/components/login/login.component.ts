e4887b5de6d8bba8a17e2a953d0e9b8c66b2f58a
aysha-fathima <fathimaayesha994@gmail.com>
2024-09-28 19:15:42 +0530

    login and register services added

--- taxiRideManagement/src/components/login/login.component.ts
+++ taxiRideManagement/src/components/login/login.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { TaxiRideService } from '../../services/taxi-ride.service';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule,RouterOutlet,FormsModule],
+  imports: [RouterModule,RouterOutlet,FormsModule,MatButtonModule,MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
